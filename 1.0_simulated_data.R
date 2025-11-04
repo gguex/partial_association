6b48018bb2012d06109b198f3104bf61b5cadf94
gguex <guillaume.guex@gmail.com>
2025-11-04 11:43:54 +0100

    Update 1.0_simulated_data.R

--- 1.0_simulated_data.R
+++ 1.0_simulated_data.R
@@ -1,2 +1,66 @@
-# 
+# ----------------------------------------------------
+# ----------------------------------------------------
+# Test the simulated data generation process
+# ----------------------------------------------------
+# ----------------------------------------------------
+
+# -------------------------------------------------
+# Source and libraries
+# -------------------------------------------------
+
+source("local_functions.R")
+library(MASS)
+
+# -------------------------------------------------
+# Parameters for the experiment
+# -------------------------------------------------
+
+set.seed(1234)
+n = 500
+p1 = 5
+p2 = 5
+r1 = 0.6
+r2 = 0.6
+r12 = 0.1
+
+# -------------------------------------------------
+# Code
+# -------------------------------------------------
+
+### Create the fixed quantities
+
+# Create the weights 
+f = runif(n)
+f = f/sum(f)
+
+# The diagonal matrix of weights
+Pi = diag(f)
+
+# The centering matrix
+H = diag(n) - outer(rep(1, n), f)
+
+### Computations
+
+# Create the covariance matrix
+Cov_mat = generate_cov2dataset(p1, p2, r1, r2, r12)
+
+# Generate the dataset
+data_sim = mvrnorm(n = n, mu = rep(0, 10), Sigma = Cov_mat)
+
+# Split the dataset
+X = data_sim[, 1:p1]
+Y = data_sim[, (p1+1):(p1+p2)]
+
+
+# Compute the kernels 
+X_s = sqrt(f)*t(t(X) - colSums(f*X))
+K_X = 0.5 * X_s %*% t(X_s)
+Y_s = sqrt(f)*t(t(Y) - colSums(f*Y))
+K_Y = 0.5 * Y_s %*% t(Y_s)
+
+
+
+
+
+
 
--- local_functions.R
+++ local_functions.R
@@ -1,31 +1,30 @@
 # ----------------------------------------------------
-#
+# ----------------------------------------------------
 # Functions for the projects
-#
+# ----------------------------------------------------
 # ----------------------------------------------------
 
-library(MASS)
+# -------------------------------------------------
+# Generate the covariance matrix for 2 datasets 
+# -------------------------------------------------
 
-# Function to generate 2 datasets 
-
-generate_cov2dataset = function(n1, n2, r1, r2, r12, 
+generate_cov2dataset = function(p1, p2, r1, r2, r12, 
                                 s1_min=0, s1_max=1, s2_min=0, s2_max=1) {
   
   # Create correlation matrix
-  R1 = matrix(r1, nrow = n1, ncol = n1)
+  R1 = matrix(r1, nrow = p1, ncol = p1)
   diag(R1) = 1
-  R2 = matrix(r2, nrow = n2, ncol = n2)
+  R2 = matrix(r2, nrow = p2, ncol = p2)
   diag(R2) = 1
-  R12 = matrix(r12, nrow = n1, ncol = n2)
+  R12 = matrix(r12, nrow = p1, ncol = p2)
   
   R = rbind( cbind(R1, R12), cbind(t(R12), R2) )
   
   # Generate variances
-  s1 = runif(n1, s1_min, s1_max)
-  s2 = runif(n2, s2_min, s2_max)
+  s1 = runif(p1, s1_min, s1_max)
+  s2 = runif(p2, s2_min, s2_max)
   
+  # Create the covariance matrix and return it
   Cov = R * outer(c(s1, s2), c(s1, s2))
-  
   return(Cov)
-  
 }
