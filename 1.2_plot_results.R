e8f17898ae58bcb7d6b0e298f33f00c7511ddb6f
gguex <guillaume.guex@gmail.com>
2025-11-06 16:42:47 +0100

    plot figures 1.1 and 1.2

--- 1.2_plot_results.R
+++ 1.2_plot_results.R
@@ -8,48 +8,59 @@ library(tidyverse)
 
 # --- Parameters
 
-r_w_v = seq(0, 1, 0.1)
+r_b_vec = seq(0, 1, 0.1)
 
 # --- Load data
 
 df = data.frame()
 
-for(r_w in r_w_v){
-  results_path = paste0("results_csv/res_2kernels_rw_", r_w, ".csv")
+for(r_b in r_b_vec){
+  results_path = paste0("results_csv/res_2kernels_rb_", r_b, ".csv")
   temp_df = read_csv(results_path) 
   df = rbind(df, temp_df)
 }
 
-# --- Summarise data
+df = df %>%
+  mutate(z_score = (C_XY -E_C_XY)/sqrt(Var_C_XY))
+  
+# --- Graph 1.1
 
-summ_df = df %>% 
+df_g1 = df %>%
+  filter(r12==0)
+
+z_means = df_g1 %>%
+  group_by(r1) %>%
+  summarise(mean_z = mean(z_score)) %>%
+  ungroup() %>%
+  .$mean_z
+
+df_g1 %>%
+  ggplot(aes(x=z_score)) +
+  geom_histogram(aes(y=..density..), bins=30, fill="lightblue", color="black") +
+  geom_vline(data=data.frame(r1=unique(df_g1$r1), mean_z=z_means), 
+             aes(xintercept=mean_z), color="red", linetype="dashed", size=1) +
+  labs(x = "z-score of Dissimilarity Covariance", 
+       y = "Density", 
+       title = "Histogram of z-scores when r_b = 0") +
+  facet_wrap(~r1, labeller = labeller(r1 = function(x) paste0("r_w", "=", x) ))
+ggsave("results_plot/zscore_histogram_2kernels.png", width=8, height=6)
+
+# --- Graph 1.2
+
+df_g2 = df %>%
+  #filter(semipos_def==1) %>%
   group_by(r1, r12) %>%
-  summarise(n = n(),
-            mean_C = mean(C_XY),
-            sd_C = sd(C_XY),
-            mean_C_th = mean(E_C_XY),
-            sd_C_th = sd(Var_C_XY))
+  summarise(mean_z = mean(z_score),
+            q5 = quantile(z_score, 0.05),
+            q95 = quantile(z_score, 0.95)) %>%
+  ungroup()
 
-# --- Plot the two curves with confidence intervals
-
-ggplot(summ_df) +
-  geom_line(aes(x = r12, y = mean_C, color = bquote("Empirical Cov")), size=1) +
-  geom_ribbon(aes(x = r12, 
-                  ymin = mean_C - 1.96*sd_C/sqrt(n), 
-                  ymax = mean_C + 1.96*sd_C/sqrt(n)), 
-              alpha = 0.2, fill="blue") +
-  geom_line(aes(x = r12, y = mean_C_th, color = "Theoretical Cov"), size=1) +
-  geom_ribbon(aes(x = r12, 
-                  ymin = mean_C_th - 1.96*sd_C_th, 
-                  ymax = mean_C_th + 1.96*sd_C_th), 
-              alpha = 0.2, fill="red") +
-  labs(x = bquote("Correlation between datasets ("~r[b]~")"), 
-       y = "Dissimilariy Covariance", 
-       title = "Dissimilarity Covariance between 2 Kernels") +
-  scale_color_manual(values = c("Empirical Cov" = "blue", "Theoretical Cov" = "red")) +
-  facet_wrap(~r1, labeller = labeller(r1 = function(x) paste0("r_w", "=", x) )) +
-  theme(legend.title=element_blank())
-ggsave("results_plot/dissimilarity_covariance_2Kernels.png", width=8, height=6)
-
-
-            
+df_g2 %>%
+  ggplot(aes(x=r12, y=mean_z)) +
+  geom_line(color="blue", size=1) +
+  geom_ribbon(aes(ymin=q5, ymax=q95), alpha=0.8, fill="lightblue") +
+  labs(x = bquote("Correlation between datasets ("~r[b]~")"),
+       y = "Mean z-score of Dissimilarity Covariance", 
+       title = "Mean z-score of Dissimilarity Covariance vs r_b") +
+  facet_wrap(~r1, labeller = labeller(r1 = function(x) paste0("r_w", "=", x) ))
+ggsave("results_plot/zscore_evo_2kernels.png", width=8, height=6)
